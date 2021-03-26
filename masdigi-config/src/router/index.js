8f03877fe8bc5b61f245e5b0b84d81fdc5978a76
rikad <rikadscreamo@gmail.com>
2021-03-26 20:31:04 +0700

    remove unused

--- masdigi-config/src/router/index.js
+++ masdigi-config/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Config from '../views/Config.vue'
 import Umum from '../components/Umum.vue'
 import Pengumuman from '../components/Pengumuman.vue'
@@ -22,21 +21,6 @@ const routes = [
       { path: 'pengumuman', component: Pengumuman },
       { path: 'praytime', component: PrayTime }
     ]
-  },
-  {
-    path: '/config',
-    children: [
-      // Note we provide the above parent route name on the default child tab
-      // route to ensure this tab is rendered by default when using named routes
-      { path: '', component: Umum, name: 'config' },
-      { path: 'galeri', component: Galeri },
-      { path: 'pengumuman', component: Pengumuman },
-      { path: 'praytime', component: PrayTime }
-    ],
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Config
   }
 ]
 
